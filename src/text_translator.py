6632c9c580a3738d3f99738e7813e85ab33b59ec
Mitya01 <dimitrimatyukov@yandex.ru>
2025-04-05 12:52:09 +0300

    [feat]: added a text translator and package deep-translator(#3)

---
+++ src/text_translator.py
@@ -0,0 +1,26 @@
+from deep_translator import GoogleTranslator
+
+
+class TextTranslator:
+    def __init__(self):
+        pass
+
+    def translate_text(self, text):
+        """
+        Переводит текст с помощью Google Translator.
+        :param text: Текст для перевода.
+        :return: Переведенный текст.
+        """
+        try:
+            translator = GoogleTranslator(source='en', target='ru')
+            return translator.translate(text)
+        except Exception as e:
+            print(f"Ошибка перевода: {e}")
+            return None
+
+# Пример использования:
+if __name__ == "__main__":
+    translator = TextTranslator()
+    original_text = "Hi, how are you?"
+    translated_text = translator.translate_text(original_text)
+    print(f"Перевод: {translated_text}")
